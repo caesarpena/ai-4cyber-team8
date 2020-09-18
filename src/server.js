dcb22252e3fb347e0d754f18abe390f4519f1847
Cesar Pena <you@example.comcesar_raynell@hotmail.com>
2020-09-18 10:57:51 -0400

    conditioning app for heroku

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/ai-4cyber-team8-prod'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/ai-4cyber-team8-prod/index.html'));});
+app.listen(process.env.PORT || 8080);
